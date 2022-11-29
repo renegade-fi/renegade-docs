c29299280307645f6c5725e20762325abc0fab5b
Christopher Bender <chrisbender@berkeley.edu>
2022-11-28 18:20:14 -0800

    Implemented landing page for desktop.

--- renegade.fi/src/index.tsx
+++ renegade.fi/src/index.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import mixpanel from 'mixpanel-browser'
 
-import logoLight from './icons/logo_light.svg'
+import './animations.css'
+import './fonts.css'
+
 import logoDark from './icons/logo_dark.svg'
+import logoDarkRotated from './icons/logo_dark_rotated.svg'
+import backgroundPattern from './icons/background_pattern.svg'
 
 import {
   BrowserRouter,
@@ -16,10 +20,11 @@ import {
   ChakraProvider,
   ColorModeScript,
   extendTheme,
-  useColorMode,
   Box,
-  Center,
+  Flex,
   Image,
+  Link,
+  Text,
   type ThemeConfig
 } from '@chakra-ui/react'
 
@@ -33,14 +38,203 @@ const config: ThemeConfig = {
 const styles = {
   global: {
     body: {
-      fontFamily: 'Montserrat',
+      fontFamily: 'Favorit Extended',
       fontWeight: '400',
-      bg: 'white',
-      textAlign: 'center'
+      fontSize: '1.4rem',
+      bg: 'black'
     }
   }
 }
 const theme = extendTheme({ config, styles })
+const linksThinUnderline = {
+  '.links:hover &': {
+    textDecorationThickness: '0.05em'
+  }
+}
+
+function LandingPageDesktop () {
+  return (
+    <Flex
+      w="100vw"
+      maxW="100%"
+      h="100vh"
+      bg="black"
+      padding="60px"
+    >
+      <Box h="100%" paddingRight="50px">
+        <Image
+          src={logoDarkRotated}
+          alt="Renegade Logo"
+          h="100%"
+        />
+      </Box>
+      <Box flexGrow="1">
+        <Flex
+          w="100%"
+          h="100%"
+          position="relative"
+          border="7px solid white"
+          backgroundImage={backgroundPattern}
+          backgroundSize="cover"
+          backgroundPosition="center"
+          backgroundRepeat="no-repeat"
+        >
+          <Box margin="auto">
+            <Flex
+              flexDirection="row"
+              justifyContent="space-between"
+            >
+              <Box
+                fontFamily="Aime"
+                fontWeight="700"
+                fontStyle="normal"
+                fontSize="6rem"
+                letterSpacing="-1px"
+                lineHeight="90%"
+                className="fade-in-right"
+              >
+                <Text>On.</Text>
+                <Text>Chain.</Text>
+                <Text>Dark.</Text>
+                <Text>Pool.</Text>
+              </Box>
+              <Flex
+                flexDirection="column"
+                justifyContent="space-between"
+                alignItems="end"
+                lineHeight="125%"
+                className="links"
+              >
+                <Flex
+                  flexDirection="column"
+                  alignItems="end"
+                  fontWeight="300"
+                  className="fade-in-left-first"
+                >
+                  <Link
+                    isExternal
+                    fontWeight="700"
+                    href="TODO"
+                  >
+                    Get Network Access
+                  </Link>
+                  <Link
+                    isExternal
+                    sx={linksThinUnderline}
+                    href="https://whitepaper.renegade.fi"
+                  >
+                    See the Whitepaper
+                  </Link>
+                  <Link
+                    isExternal
+                    sx={linksThinUnderline}
+                    href="https://docs.renegade.fi"
+                  >
+                    Read the Docs
+                  </Link>
+                </Flex>
+                <Box
+                  className="fade-in-left-second"
+                >
+                  <Text fontWeight="700">
+                    Our Investors
+                  </Text>
+                  <Flex
+                    flexDirection="column"
+                    alignItems="end"
+                    fontWeight="300"
+                    fontSize="1rem"
+                    opacity="80%"
+                    lineHeight="125%"
+                  >
+                    <Link
+                      isExternal
+                      sx={linksThinUnderline}
+                      href="https://twitter.com/dragonfly_xyz"
+                    >
+                      Dragonfly Capital
+                    </Link>
+                    <Link
+                      isExternal
+                      sx={linksThinUnderline}
+                      href="https://twitter.com/naval"
+                    >
+                      Naval Ravikant
+                    </Link>
+                    <Link
+                      isExternal
+                      sx={linksThinUnderline}
+                      href="https://twitter.com/balajis"
+                    >
+                      Balaji Srinivasan
+                    </Link>
+                    <Link
+                      isExternal
+                      sx={linksThinUnderline}
+                      href="https://twitter.com/robotventures"
+                    >
+                      Robot Ventures
+                    </Link>
+                    <Link
+                      isExternal
+                      sx={linksThinUnderline}
+                      href="https://symbolic.partners"
+                    >
+                      Symbolic Partners
+                    </Link>
+                  </Flex>
+                </Box>
+                <Flex
+                  flexDirection="column"
+                  alignItems="end"
+                  fontWeight="300"
+                  paddingBottom="8px"
+                  className="fade-in-left-third"
+                >
+                  <Link
+                    isExternal
+                    fontWeight="700"
+                    href="https://twitter.com/renegade_fi"
+                  >
+                    Follow on Twitter
+                  </Link>
+                  <Link
+                    isExternal
+                    sx={linksThinUnderline}
+                    href="https://renegadefi.substack.com"
+                  >
+                    Read our Substack
+                  </Link>
+                  <Link
+                    isExternal
+                    sx={linksThinUnderline}
+                    href="https://jobs.renegade.fi"
+                  >
+                    Work with Us
+                  </Link>
+                </Flex>
+              </Flex>
+            </Flex>
+            <Box
+              fontWeight="300"
+              fontSize="0.8rem"
+              letterSpacing="20px"
+              marginRight="-20px"
+              paddingTop="15px"
+              className="fade-in-compress"
+            >
+              EVERYWHERE NOWHERE
+            </Box>
+          </Box>
+        </Flex>
+      </Box>
+    </Flex>
+  )
+}
+
+function LandingPage () {
+  return <LandingPageDesktop />
+}
 
 interface ExternalNavigateProps {
   to: string
@@ -55,38 +249,6 @@ class ExternalNavigate extends React.Component<ExternalNavigateProps> {
   }
 }
 
-function App () {
-  const { colorMode } = useColorMode()
-  const logo = colorMode === 'light' ? logoLight : logoDark
-  return (
-    <Box
-      w="100vw"
-      maxW="100%"
-      h="100vh"
-      bg={
-        colorMode === 'light'
-          ? 'white'
-          : 'radial-gradient(farthest-side circle, #202020, #111111)'
-      }
-      backgroundAttachment="fixed"
-    >
-      <Center>
-        <Image
-          src={logo}
-          position="absolute"
-          margin="auto"
-          top="0"
-          bottom="0"
-          right="0"
-          left="0"
-          transform="translateY(-60%)"
-          w={['70%', '35%']}
-        />
-      </Center>
-    </Box>
-  )
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
@@ -94,7 +256,7 @@ root.render(
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<App />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/whitepaper" element={<ExternalNavigate to="/whitepaper.pdf" />} />
           <Route path="/*" element={<Navigate to="/" />} />
         </Routes>
