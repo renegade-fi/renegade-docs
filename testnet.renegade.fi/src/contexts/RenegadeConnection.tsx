4851f4ee2ce64502aafcedd10a988528b0100542
Christopher Bender <chrisbender@berkeley.edu>
2023-02-16 16:30:57 -0800

    testnet.renegade.fi: Fixed type for RenegadeConnectionContext.

--- testnet.renegade.fi/src/contexts/RenegadeConnection.tsx
+++ testnet.renegade.fi/src/contexts/RenegadeConnection.tsx
@@ -2,13 +2,9 @@ import React from "react";
 
 import RenegadeConnection from "../connections/RenegadeConnection";
 
-type RenegadeConnectionContextType = [
-  RenegadeConnection,
-  React.Dispatch<React.SetStateAction<RenegadeConnection>>,
-];
+type RenegadeConnectionContextType = RenegadeConnection;
 const RenegadeConnectionContext =
-  React.createContext<RenegadeConnectionContextType>([
+  React.createContext<RenegadeConnectionContextType>(
     RenegadeConnection.default(),
-    () => null,
-  ]);
+  );
 export default RenegadeConnectionContext;
