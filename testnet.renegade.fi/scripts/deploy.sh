4f30931f3d2e64afaa4efbb8a80ebe86303798f1
Sehyun Chung <sehyun@berkeley.edu>
2023-07-20 13:44:28 -0700

    testnet: initial commit, delete old testnet

--- testnet.renegade.fi/scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-if [ "$1" == "--dev" ]; then
-    aws s3 sync dist/ s3://stage.testnet.renegade.fi --delete --exclude "*.sw[a-p]"
-    aws cloudfront create-invalidation --distribution-id E3KVSGFA1XRH1R --paths "/*"
-fi
-if [ "$1" == "--prod" ]; then
-    # aws s3 sync dist/ s3://testnet.renegade.fi --delete --exclude "*.sw[a-p]"
-    # aws cloudfront create-invalidation --distribution-id E182E1ZLY7OQGA --paths "/*"
-fi
