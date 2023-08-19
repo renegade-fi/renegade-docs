3341c830256f959e3e50be3bac3daf93a93678c7
Sehyun Chung <sehyun@berkeley.edu>
2023-08-19 15:27:16 -0700

    testnet: test nested routing maintaining state

--- testnet.renegade.fi/app/layout.tsx
+++ testnet.renegade.fi/app/layout.tsx
@@ -1,8 +1,13 @@
 import React from "react"
 import type { Metadata } from "next"
+import { env } from "@/env.mjs"
+import { HealthStates } from "@/types"
+import { Renegade, Token } from "@renegade-fi/renegade-js"
 
+import { DISPLAYED_TICKERS } from "@/lib/tokens"
 import Footer from "@/components/footer"
 import MainNav from "@/components/main-nav"
+import TokensBanner from "@/app/[base]/[quote]/tokens-banner"
 import { Providers } from "@/app/providers"
 
 // TODO: merge globals.css and index.css
@@ -16,11 +21,41 @@ export const metadata: Metadata = {
   description: "Renegade Testnet",
 }
 
-export default function RootLayout({
+const renegade = new Renegade({
+  relayerHostname: env.NEXT_PUBLIC_RENEGADE_RELAYER_HOSTNAME,
+  relayerHttpPort: 3000,
+  relayerWsPort: 4000,
+  useInsecureTransport:
+    env.NEXT_PUBLIC_NODE_ENV === "development" ? true : false,
+  verbose: false,
+})
+
+const tokensBannerRes: Promise<HealthStates>[] = []
+DISPLAYED_TICKERS.forEach(
+  async ([baseTicker, quoteTicker]: [string, string]) => {
+    tokensBannerRes.push(
+      renegade?.queryExchangeHealthStates(
+        new Token({ ticker: baseTicker }),
+        new Token({ ticker: quoteTicker })
+      )
+    )
+  }
+)
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const tokensBannerHealthStates = await Promise.all(tokensBannerRes)
+
+  const initialTokenPrices = tokensBannerHealthStates.map((healthState) => {
+    return (
+      healthState.median.DataTooStale?.[0] ||
+      healthState.median.Nominal ||
+      healthState.median.TooMuchDeviation?.[0]
+    )
+  })
   return (
     <html lang="en">
       <body>
@@ -36,6 +71,7 @@ export default function RootLayout({
           >
             <MainNav />
             {children}
+            <TokensBanner initialTokenPrices={initialTokenPrices} />
             <Footer />
           </div>
         </Providers>
