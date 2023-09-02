4573775809db20fa31598ca21938fc8064bb4a90
Sehyun Chung <sehyun@berkeley.edu>
2023-09-01 17:21:26 -0700

    testnet: Add listeners cleanup functions, add network listener

--- testnet.renegade.fi/contexts/Order/order-context.tsx
+++ testnet.renegade.fi/contexts/Order/order-context.tsx
@@ -89,9 +89,9 @@ function OrderProvider({ children }: OrderProviderProps) {
     }
     handleNetworkListener()
     return () => {
-      if (orderCallbackId.current) {
-        renegade.releaseCallback(orderCallbackId.current)
-      }
+      if (!orderCallbackId.current) return
+      renegade.releaseCallback(orderCallbackId.current)
+      orderCallbackId.current = undefined
     }
   }, [])
 
