38b1d5bcf04947f25f23f007083706ccfd9ec1c9
Sehyun Chung <sehyun@berkeley.edu>
2023-09-20 14:20:54 -0700

    testnet: Add Sentry error monitoring

--- testnet.renegade.fi/next.config.mjs
+++ testnet.renegade.fi/next.config.mjs
@@ -1,5 +1,6 @@
+import { withSentryConfig } from "@sentry/nextjs"
+
 import "./env.mjs"
-import analyzer from "@next/bundle-analyzer"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -12,4 +13,34 @@ const nextConfig = {
   },
 }
 
-export default analyzer({ enabled: process.env.ANALYZE === "true" })(nextConfig)
+export default withSentryConfig(
+  nextConfig,
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+    org: "renegade-finance-fae03428c",
+    project: "javascript-nextjs",
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: "/monitoring",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  }
+)
