acd64650a2a6dec7fca57a252d50ef63fee14a8f
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2023-11-09 13:40:28 -0800

    add old median banner

--- testnet.renegade.fi/components/banners/median-banner.tsx
+++ testnet.renegade.fi/components/banners/median-banner.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useOrder } from "@/contexts/Order/order-context"
+import React from "react"
 import { Box, Flex, Link, Spacer, Stack, Text } from "@chakra-ui/react"
 import {
   Exchange,
@@ -12,74 +12,38 @@ import {
 import { TICKER_TO_ADDR } from "@/lib/tokens"
 import { BannerSeparator } from "@/components/banners/banner-separator"
 import { LivePrices } from "@/components/banners/live-price"
-import { InteractiveMarquee } from "@/components/banners/marquee"
 import { PulsingConnection } from "@/components/banners/pulsing-connection-indicator"
 
-export function MedianBanner({ report }: { report: ExchangeHealthState }) {
+function LinkWrapper(props: { link?: string; children: React.ReactNode }) {
   return (
     <Flex
+      as={Link}
       alignItems="center"
-      style={{
-        borderBottom: "var(--border)",
-      }}
+      justifyContent="center"
+      flexGrow="1"
+      height="100%"
+      _hover={{ textDecoration: "none" }}
+      userSelect="none"
+      cursor={props.link ? undefined : "inherit"}
+      href={props.link}
+      isExternal
     >
-      <Flex
-        alignItems="center"
-        justifyContent="center"
-        width="24%"
-        minWidth="400px"
-        height="100%"
-      >
-        <Spacer flexGrow="3" />
-        <Text whiteSpace="nowrap">NBBO Feed</Text>
-        <BannerSeparator flexGrow={4} />
-        <ExchangeConnectionTriple
-          exchange={Exchange.Median}
-          priceReport={
-            report.Median || { healthState: HealthState.enum.NoDataReported }
-          }
-        />
-        <BannerSeparator flexGrow={4} />
-      </Flex>
-      <Flex position="relative">
-        <Box
-          position="absolute"
-          zIndex="1"
-          top={"1px"}
-          bottom={"1px"}
-          width={"10px"}
-          background={`linear-gradient(${"90deg"}, rgba(0,0,0,1), rgba(0,0,0,0))`}
-        />
-        <InteractiveMarquee>
-          {Object.entries(report)
-            .filter(([exchange]) => exchange !== "Median")
-            .map(([exchange, p]) => {
-              return (
-                <Flex key={exchange} alignItems="center" gap="4" marginLeft="4">
-                  <ExchangeConnectionTriple
-                    exchange={exchange.toLowerCase() as Exchange}
-                    priceReport={p}
-                  />
-                  <BannerSeparator flexGrow={4} />
-                </Flex>
-              )
-            })}
-        </InteractiveMarquee>
-      </Flex>
+      {props.children}
     </Flex>
   )
 }
 
 interface ExchangeConnectionTripleProps {
+  activeBaseTicker: string
+  activeQuoteTicker: string
   exchange: Exchange
-  isMobile?: boolean
   priceReport: PriceReport
+  isMobile?: boolean
 }
-export function ExchangeConnectionTriple(props: ExchangeConnectionTripleProps) {
-  const { baseTicker, quoteTicker } = useOrder()
+function ExchangeConnectionTriple(props: ExchangeConnectionTripleProps) {
   // Remap some tickers, as different exchanges use different names
-  let renamedBaseTicker = baseTicker
-  let renamedQuoteTicker = quoteTicker
+  let renamedBaseTicker = props.activeBaseTicker
+  let renamedQuoteTicker = props.activeQuoteTicker
   if (renamedBaseTicker === "WBTC") {
     renamedBaseTicker = "BTC"
   }
@@ -101,11 +65,13 @@ export function ExchangeConnectionTriple(props: ExchangeConnectionTripleProps) {
     coinbase: `https://www.coinbase.com/advanced-trade/${renamedBaseTicker}-${renamedQuoteTicker}`,
     kraken: `https://pro.kraken.com/app/trade/${renamedBaseTicker}-${renamedQuoteTicker}`,
     okx: `https://www.okx.com/trade-swap/${renamedBaseTicker}-${renamedQuoteTicker}-swap`,
-    uniswapv3: `https://info.uniswap.org/#/tokens/${TICKER_TO_ADDR[baseTicker]}`,
+    uniswapv3: `https://info.uniswap.org/#/tokens/${
+      TICKER_TO_ADDR[props.activeBaseTicker]
+    }`,
     median: "",
   }[props.exchange]
-  const healthState = props.priceReport?.healthState
 
+  let healthState = props.priceReport.healthState
   let showPrice: boolean
   let connectionText: string
   let textVariant: string
@@ -148,52 +114,393 @@ export function ExchangeConnectionTriple(props: ExchangeConnectionTripleProps) {
   }[textVariant] as "live" | "loading" | "dead"
 
   return (
-    <Flex
-      as={Link}
-      alignItems="center"
-      gap="2"
-      _hover={{ textDecoration: "none" }}
-      userSelect="none"
-      cursor="pointer"
-      href={link}
-      isExternal
-    >
-      <Text variant={props.isMobile ? "rotate-right" : undefined}>
-        {props.exchange[0].toUpperCase() + props.exchange.slice(1)}
-      </Text>
+    <>
+      <LinkWrapper link={link}>
+        <Text variant={props.isMobile ? "rotate-right" : undefined}>
+          {props.exchange[0].toUpperCase() + props.exchange.slice(1)}
+        </Text>
+      </LinkWrapper>
+      {/* TODO: Add Link */}
       <BannerSeparator flexGrow={1} />
       {showPrice && (
         <LivePrices
-          baseTicker={baseTicker}
-          quoteTicker={quoteTicker}
+          baseTicker={props.activeBaseTicker}
+          quoteTicker={props.activeQuoteTicker}
           exchange={props.exchange}
           isMobile={props.isMobile}
-          price={props.priceReport?.midpointPrice}
+          price={props.priceReport.midpointPrice}
         />
       )}
-      <Stack
-        alignItems="baseline"
-        justifyContent="center"
-        direction={props.isMobile ? "column" : "row"}
-        whiteSpace="nowrap"
-        spacing={props.isMobile ? "8px" : "1"}
-      >
-        <Text
-          sx={
-            props.isMobile
-              ? {
-                  writingMode: "vertical-rl",
-                  textOrientation: "sideways",
-                }
-              : undefined
-          }
-          lineHeight="1"
-          variant={textVariant}
+      <LinkWrapper link={link}>
+        <Stack
+          alignItems="center"
+          justifyContent="center"
+          direction={props.isMobile ? "column" : "row"}
+          spacing={props.isMobile ? "8px" : "5px"}
         >
-          {connectionText}
-        </Text>
-        <PulsingConnection state={pulseState} />
-      </Stack>
+          <Text
+            sx={
+              props.isMobile
+                ? {
+                    writingMode: "vertical-rl",
+                    textOrientation: "sideways",
+                  }
+                : undefined
+            }
+            lineHeight="1"
+            variant={textVariant}
+          >
+            {connectionText}
+          </Text>
+          <PulsingConnection state={pulseState} />
+        </Stack>
+      </LinkWrapper>
+    </>
+  )
+}
+
+interface MedianTripleProps {
+  activeBaseTicker: string
+  activeQuoteTicker: string
+  priceReport: PriceReport
+  isMobile?: boolean
+}
+function MedianTriple(props: MedianTripleProps) {
+  return (
+    <Flex
+      alignItems="center"
+      justifyContent="center"
+      flexDirection={props.isMobile ? "column" : "row"}
+      width={props.isMobile ? "100%" : "24%"}
+      minWidth={props.isMobile ? undefined : "400px"}
+      height={props.isMobile ? "40%" : "100%"}
+      paddingTop={props.isMobile ? "10px" : undefined}
+    >
+      <Spacer flexGrow="3" />
+      <Text
+        whiteSpace="nowrap"
+        variant={props.isMobile ? "rotate-right" : undefined}
+      >
+        NBBO Feed
+      </Text>
+      <BannerSeparator flexGrow={4} />
+      <ExchangeConnectionTriple
+        activeBaseTicker={props.activeBaseTicker}
+        activeQuoteTicker={props.activeQuoteTicker}
+        exchange={Exchange.Median}
+        isMobile={props.isMobile}
+        priceReport={props.priceReport}
+      />
+      <BannerSeparator flexGrow={4} />
     </Flex>
   )
+}
+
+interface ExchangeConnectionsBannerProps {
+  activeBaseTicker: string
+  activeQuoteTicker: string
+  isMobile?: boolean
+  report: ExchangeHealthState
+}
+interface ExchangeConnectionsBannerState {
+  exchangeConnectionsBannerRef: React.RefObject<HTMLDivElement>
+  // priceReporterHealthStates: {
+  //   [exchange: string]: HealthState
+  // }
+  isTooShort: boolean
+  isScrolling: boolean
+  scrollDirection: "left" | "right"
+  isHovered: boolean
+  isClicked: boolean
+  mouseDownX: number
+}
+export class MedianBanner extends React.Component<
+  ExchangeConnectionsBannerProps,
+  ExchangeConnectionsBannerState
+> {
+  constructor(props: ExchangeConnectionsBannerProps) {
+    super(props)
+    this.state = this.defaultState()
+    // this.checkExchangeHealthStates = this.checkExchangeHealthStates.bind(this)
+    this.performScroll = this.performScroll.bind(this)
+    this.onMouseEnter = this.onMouseEnter.bind(this)
+    this.onMouseLeave = this.onMouseLeave.bind(this)
+    this.onMouseDown = this.onMouseDown.bind(this)
+    this.onMouseUp = this.onMouseUp.bind(this)
+    this.onMouseMove = this.onMouseMove.bind(this)
+  }
+
+  async componentDidMount() {
+    // Periodically check for health, setting live/dead appropriately
+    // this.checkExchangeHealthStates()
+    // Add listeners for mouse events
+    window.addEventListener("mouseup", this.onMouseUp)
+    // @ts-ignore
+    window.addEventListener("mousemove", this.onMouseMove)
+    // Animate scroll if banner is compressed
+    this.performScroll()
+  }
+
+  componentDidUpdate(prevProps: ExchangeConnectionsBannerProps) {
+    if (
+      prevProps.activeBaseTicker === this.props.activeBaseTicker &&
+      prevProps.activeQuoteTicker === this.props.activeQuoteTicker
+    ) {
+      return
+    }
+    this.setState(this.defaultState())
+  }
+
+  defaultState(): ExchangeConnectionsBannerState {
+    return {
+      exchangeConnectionsBannerRef: React.createRef(),
+      isTooShort: false,
+      isScrolling: true,
+      scrollDirection: "left",
+      isHovered: false,
+      isClicked: false,
+      mouseDownX: 0,
+    }
+  }
+
+  // async checkExchangeHealthStates() {
+  //   if (this.props.activeBaseTicker === this.props.activeQuoteTicker) {
+  //     return
+  //   }
+  //   const healthStates = await renegade.queryExchangeHealthStates(
+  //     new Token({ ticker: this.props.activeBaseTicker }),
+  //     new Token({ ticker: this.props.activeQuoteTicker })
+  //   )
+  //   function getHealthState(
+  //     priceReport: string | Record<string, PriceReport>
+  //   ): HealthState {
+  //     if (!priceReport || priceReport === "Unsupported") {
+  //       return "unsupported"
+  //     }
+  //     if (priceReport["Nominal"] !== undefined) {
+  //       return "live"
+  //     }
+  //     if (priceReport === "NoDataReported") {
+  //       return "no-data"
+  //     }
+  //     if (priceReport["DataTooStale"] !== undefined) {
+  //       return "too-stale"
+  //     }
+  //     if (priceReport["NotEnoughDataReported"] !== undefined) {
+  //       return "not-enough-data"
+  //     }
+  //     if (priceReport["TooMuchDeviation"] !== undefined) {
+  //       return "too-much-deviation"
+  //     }
+  //     throw new Error("Invalid priceReport: " + priceReport)
+  //   }
+  //   const newPriceReporterHealthStates = {
+  //     median: getHealthState(healthStates["median"]),
+  //     binance: getHealthState(healthStates["all_exchanges"]["Binance"]),
+  //     coinbase: getHealthState(healthStates["all_exchanges"]["Coinbase"]),
+  //     kraken: getHealthState(healthStates["all_exchanges"]["Kraken"]),
+  //     okx: getHealthState(healthStates["all_exchanges"]["Okx"]),
+  //     uniswapv3: getHealthState(healthStates["all_exchanges"]["UniswapV3"]),
+  //   }
+  //   this.setState({
+  //     priceReporterHealthStates: newPriceReporterHealthStates,
+  //   })
+  //   setTimeout(this.checkExchangeHealthStates, 1000)
+  // }
+
+  performScroll() {
+    const exchangeConnectionsBanner =
+      this.state.exchangeConnectionsBannerRef.current
+    if (exchangeConnectionsBanner && this.state.isScrolling) {
+      let scrollDest = this.props.isMobile
+        ? exchangeConnectionsBanner.scrollTop
+        : exchangeConnectionsBanner.scrollLeft
+      scrollDest += this.state.scrollDirection === "left" ? 1 : -1
+      const maxScroll = this.props.isMobile
+        ? exchangeConnectionsBanner.scrollHeight -
+          exchangeConnectionsBanner.clientHeight
+        : exchangeConnectionsBanner.scrollWidth -
+          exchangeConnectionsBanner.clientWidth
+      this.setState({ isTooShort: maxScroll > 5 })
+      if (maxScroll > 5 && !this.state.isHovered && !this.state.isClicked) {
+        if (scrollDest <= 0) {
+          scrollDest = 0
+          this.setState({
+            isScrolling: false,
+            scrollDirection: "left",
+          })
+          setTimeout(() => this.setState({ isScrolling: true }), 1000)
+        } else if (scrollDest >= maxScroll) {
+          scrollDest = maxScroll
+          this.setState({
+            isScrolling: false,
+            scrollDirection: "right",
+          })
+          setTimeout(() => this.setState({ isScrolling: true }), 1000)
+        }
+        exchangeConnectionsBanner.scrollTo(
+          this.props.isMobile ? 0 : scrollDest,
+          this.props.isMobile ? scrollDest : 0
+        )
+      }
+    }
+    setTimeout(this.performScroll, 50)
+  }
+
+  onMouseEnter() {
+    this.setState({
+      isHovered: true,
+    })
+  }
+
+  onMouseLeave() {
+    this.setState({
+      isHovered: false,
+    })
+  }
+
+  onMouseDown(event: React.MouseEvent) {
+    this.setState({
+      isClicked: true,
+      mouseDownX: event.clientX,
+    })
+  }
+
+  onMouseUp() {
+    this.setState({
+      isClicked: false,
+    })
+  }
+
+  onMouseMove(event: React.MouseEvent) {
+    const exchangeConnectionsBanner =
+      this.state.exchangeConnectionsBannerRef.current
+    if (exchangeConnectionsBanner && this.state.isClicked) {
+      exchangeConnectionsBanner.scrollBy(
+        -event.movementX / window.devicePixelRatio,
+        0
+      )
+    }
+  }
+
+  render() {
+    console.log(this.props)
+    return (
+      <Stack
+        alignItems="center"
+        justifyContent="flex-start"
+        direction={this.props.isMobile ? "column" : "row"}
+        width={
+          this.props.isMobile ? "calc(0.75 * var(--banner-height))" : "100%"
+        }
+        height={this.props.isMobile ? "220vw" : "var(--banner-height)"}
+        color="white.80"
+        fontSize={this.props.isMobile ? "0.8em" : undefined}
+        borderColor="border"
+        borderBottom={this.props.isMobile ? undefined : "var(--border)"}
+        borderLeft={this.props.isMobile ? "var(--border)" : undefined}
+        userSelect="none"
+        spacing="0px"
+      >
+        <MedianTriple
+          activeBaseTicker={this.props.activeBaseTicker}
+          activeQuoteTicker={this.props.activeQuoteTicker}
+          isMobile={this.props.isMobile}
+          priceReport={this.props.report.Median}
+        />
+        <Flex
+          position="relative"
+          flexDirection={this.props.isMobile ? "column" : "row"}
+          width={this.props.isMobile ? undefined : "76%"}
+          height={this.props.isMobile ? "60%" : undefined}
+        >
+          <Box
+            position="absolute"
+            zIndex="1"
+            top={this.props.isMobile ? "0px" : "1px"}
+            right={this.props.isMobile ? "1px" : undefined}
+            bottom={this.props.isMobile ? undefined : "1px"}
+            left={this.props.isMobile ? "1px" : "0px"}
+            width={this.props.isMobile ? undefined : "10px"}
+            height={this.props.isMobile ? "10px" : undefined}
+            background={`linear-gradient(${
+              this.props.isMobile ? "180deg" : "90deg"
+            }, rgba(0,0,0,1), rgba(0,0,0,0))`}
+            visibility={this.state.isTooShort ? undefined : "hidden"}
+          ></Box>
+          <Box
+            ref={this.state.exchangeConnectionsBannerRef}
+            position="relative"
+            overflowX="hidden"
+            overflowY="hidden"
+            width="100%"
+            height="100%"
+            onClick={(e) => {
+              if (
+                this.props.isMobile ||
+                Math.abs(e.clientX - this.state.mouseDownX) > 5
+              ) {
+                e.preventDefault()
+              }
+            }}
+            onDragStart={(e) => e.preventDefault()}
+            onMouseDown={this.props.isMobile ? undefined : this.onMouseDown}
+            onMouseEnter={this.props.isMobile ? undefined : this.onMouseEnter}
+            onMouseLeave={this.props.isMobile ? undefined : this.onMouseLeave}
+            onMouseMove={this.props.isMobile ? undefined : this.onMouseMove}
+            onMouseUp={this.props.isMobile ? undefined : this.onMouseUp}
+          >
+            <Flex
+              alignItems="center"
+              justifyContent="center"
+              flexDirection={this.props.isMobile ? "column" : "row"}
+              minWidth={this.props.isMobile ? undefined : "1200px"}
+              minHeight={this.props.isMobile ? "310vw" : "var(--banner-height)"}
+            >
+              <ExchangeConnectionTriple
+                activeBaseTicker={this.props.activeBaseTicker}
+                activeQuoteTicker={this.props.activeQuoteTicker}
+                exchange={Exchange.Binance}
+                priceReport={this.props.report.Binance}
+                isMobile={this.props.isMobile}
+              />
+              <BannerSeparator flexGrow={4} />
+              <ExchangeConnectionTriple
+                activeBaseTicker={this.props.activeBaseTicker}
+                activeQuoteTicker={this.props.activeQuoteTicker}
+                exchange={Exchange.Coinbase}
+                priceReport={this.props.report.Coinbase}
+                isMobile={this.props.isMobile}
+              />
+              <BannerSeparator flexGrow={4} />
+              <ExchangeConnectionTriple
+                activeBaseTicker={this.props.activeBaseTicker}
+                activeQuoteTicker={this.props.activeQuoteTicker}
+                exchange={Exchange.Kraken}
+                priceReport={this.props.report.Kraken}
+                isMobile={this.props.isMobile}
+              />
+              <BannerSeparator flexGrow={4} />
+              <ExchangeConnectionTriple
+                activeBaseTicker={this.props.activeBaseTicker}
+                activeQuoteTicker={this.props.activeQuoteTicker}
+                exchange={Exchange.Okx}
+                priceReport={this.props.report.Okx}
+                isMobile={this.props.isMobile}
+              />
+              <BannerSeparator flexGrow={4} />
+              <ExchangeConnectionTriple
+                activeBaseTicker={this.props.activeBaseTicker}
+                activeQuoteTicker={this.props.activeQuoteTicker}
+                exchange={Exchange.Uniswapv3}
+                priceReport={this.props.report.UniswapV3}
+                isMobile={this.props.isMobile}
+              />
+              <Spacer flexGrow="3" />
+            </Flex>
+          </Box>
+        </Flex>
+      </Stack>
+    )
+  }
 }
