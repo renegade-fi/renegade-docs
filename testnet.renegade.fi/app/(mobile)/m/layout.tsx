7ca82dba5906b92f60cf05fbe4f9da79e5f9dc77
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2023-11-09 18:48:07 -0800

    testnet Add mobile

---
+++ testnet.renegade.fi/app/(mobile)/m/layout.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import type { Metadata } from "next"
+
+import { Providers } from "@/app/providers"
+
+import "@/styles/animations.css"
+import "@/styles/fonts.css"
+import "@/styles/globals.css"
+import "@/styles/index.css"
+
+export const metadata: Metadata = {
+  title: {
+    default: "Trade - Renegade Testnet",
+    template: `%s - Renegade Testnet`,
+  },
+  description:
+    "The on-chain dark pool. MPC-based DEX for anonymous crosses at midpoint prices.",
+}
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
+}
