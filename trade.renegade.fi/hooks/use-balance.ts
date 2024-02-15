4ee894fa904c6aa083c3802eb4386ad16cff1b52
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-02-14 16:33:32 -0800

    revert balances hook until websocket works with sig

--- trade.renegade.fi/hooks/use-balance.ts
+++ trade.renegade.fi/hooks/use-balance.ts
@@ -1,25 +1,28 @@
+import { renegade } from "@/app/providers"
 import { useRenegade } from "@/contexts/Renegade/renegade-context"
+import { Balance, BalanceId } from "@renegade-fi/renegade-js"
+import { useEffect, useState } from "react"
 
 
 export const useBalance = () => {
-  // const [balances, setBalances] = useState<Record<BalanceId, Balance>>({})
-  // const { accountId } = useRenegade()
+  const [balances, setBalances] = useState<Record<BalanceId, Balance>>({})
+  const { accountId } = useRenegade()
 
-  // useEffect(() => {
-  //   if (!accountId) return
-  //   const interval = setInterval(async () => {
-  //     const fetchedBalances = await renegade
-  //       .queryWallet(accountId)
-  //       .then(() => renegade.getBalances(accountId))
-  //     setBalances(fetchedBalances)
-  //   }, 5000)
+  useEffect(() => {
+    if (!accountId) return
+    const interval = setInterval(async () => {
+      const fetchedBalances = await renegade
+        .queryWallet(accountId)
+        .then(() => renegade.getBalances(accountId))
+      setBalances(fetchedBalances)
+    }, 5000)
 
-  //   return () => {
-  //     clearInterval(interval)
-  //   }
-  // }, [accountId])
+    return () => {
+      clearInterval(interval)
+    }
+  }, [accountId])
 
-  // return balances
-  const { balances } = useRenegade()
   return balances
+  // const { balances } = useRenegade()
+  // return balances
 }
