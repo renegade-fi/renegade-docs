25f57ff7b8979a040d887331cdc3fc555e3ae391
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-02-14 16:37:02 -0800

    revert orders hook until websocket works with sig

--- trade.renegade.fi/hooks/use-order.ts
+++ trade.renegade.fi/hooks/use-order.ts
@@ -1,39 +1,43 @@
+import { renegade } from "@/app/providers"
 import { useRenegade } from "@/contexts/Renegade/renegade-context"
+import { safeLocalStorageGetItem, safeLocalStorageSetItem } from "@/lib/utils"
+import { Order, OrderId } from "@renegade-fi/renegade-js"
+import { useEffect, useState } from "react"
 
 
 export const useOrders = () => {
-  // const { accountId } = useRenegade()
-  // const [orders, setOrders] = useState<Record<OrderId, Order>>({})
+  const { accountId } = useRenegade()
+  const [orders, setOrders] = useState<Record<OrderId, Order>>({})
 
-  // useEffect(() => {
-  //   if (!accountId) return
-  //   const interval = setInterval(async () => {
-  //     const existingOrders = safeLocalStorageGetItem(`orders-${accountId}`)
-  //     const existingOrdersArray = existingOrders
-  //       ? existingOrders.split(",")
-  //       : []
+  useEffect(() => {
+    if (!accountId) return
+    const interval = setInterval(async () => {
+      const existingOrders = safeLocalStorageGetItem(`orders-${accountId}`)
+      const existingOrdersArray = existingOrders
+        ? existingOrders.split(",")
+        : []
 
-  //     const fetchedOrders = await renegade
-  //       .queryWallet(accountId)
-  //       .then(() => renegade.getOrders(accountId))
-  //     setOrders(fetchedOrders)
+      const fetchedOrders = await renegade
+        .queryWallet(accountId)
+        .then(() => renegade.getOrders(accountId))
+      setOrders(fetchedOrders)
 
-  //     const uniqueNewOrderIds = Object.keys(fetchedOrders).filter(
-  //       (orderId) => !existingOrdersArray.includes(orderId)
-  //     )
+      const uniqueNewOrderIds = Object.keys(fetchedOrders).filter(
+        (orderId) => !existingOrdersArray.includes(orderId)
+      )
 
-  //     existingOrdersArray.push(...uniqueNewOrderIds)
-  //     const updatedOrders = existingOrdersArray.join(",")
+      existingOrdersArray.push(...uniqueNewOrderIds)
+      const updatedOrders = existingOrdersArray.join(",")
 
-  //     safeLocalStorageSetItem(`orders-${accountId}`, updatedOrders)
-  //   }, 1000)
+      safeLocalStorageSetItem(`orders-${accountId}`, updatedOrders)
+    }, 1000)
 
-  //   return () => {
-  //     clearInterval(interval)
-  //   }
-  // }, [accountId])
+    return () => {
+      clearInterval(interval)
+    }
+  }, [accountId])
 
-  // return orders
-  const { orders } = useRenegade()
   return orders
+  // const { orders } = useRenegade()
+  // return orders
 }
