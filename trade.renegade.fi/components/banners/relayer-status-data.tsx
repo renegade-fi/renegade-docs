9f09baea94c45f7cae696852e01393162cbca47d
Sehyun Chung ✌︎ <41171808+sehyunc@users.noreply.github.com>
2024-06-06 15:50:41 -0700

    trade: use ping query

--- trade.renegade.fi/components/banners/relayer-status-data.tsx
+++ trade.renegade.fi/components/banners/relayer-status-data.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import { usePing } from "@renegade-fi/react"
 import { useLocalStorage } from "usehooks-ts"
 
 import { useTvl } from "@/hooks/use-tvl"
@@ -8,7 +8,6 @@ import { useTvl } from "@/hooks/use-tvl"
 import { RelayerStatusBanner } from "@/components/banners/relayer-banner"
 
 export function RelayerStatusData() {
-  const [ping, setPing] = useState("loading")
   const [base] = useLocalStorage("base", "WETH", { initializeWithValue: false })
   const [quote] = useLocalStorage("quote", "USDC", {
     initializeWithValue: false,
@@ -16,36 +15,17 @@ export function RelayerStatusData() {
   const baseTvl = useTvl(base)
   const quoteTvl = useTvl(quote)
 
-  useEffect(() => {
-    const fetchPing = async () => {
-      try {
-        const response = await fetch(
-          `${
-            process.env.NEXT_PUBLIC_RENEGADE_RELAYER_HOSTNAME === "localhost"
-              ? "http"
-              : "https"
-          }://${process.env.NEXT_PUBLIC_RENEGADE_RELAYER_HOSTNAME}:3000/v0/ping`
-        )
-        if (response.ok) {
-          setPing("live")
-        } else {
-          setPing("dead")
-        }
-      } catch (error) {
-        setPing("dead")
-      }
-    }
-
-    fetchPing()
-    const intervalId = setInterval(fetchPing, 60000) // Set interval to 60 seconds
-
-    return () => clearInterval(intervalId) // Cleanup interval on component unmount
-  }, [])
+  const { data: ping, status } = usePing()
+  const connectionState =
+    status === "pending"
+      ? "loading"
+      : status === "error" || !ping
+      ? "dead"
+      : "live"
 
   return (
     <RelayerStatusBanner
-      // @ts-ignore
-      connectionState={ping}
+      connectionState={connectionState}
       activeBaseTicker={base}
       activeQuoteTicker={quote}
       baseTvl={baseTvl}
