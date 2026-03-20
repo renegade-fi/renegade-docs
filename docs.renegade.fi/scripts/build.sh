2c1a0f9e5cb7aefa50d6a8a42ae708263c6753db
Koh Wei Jie <contact@kohweijie.com>
2026-03-20 11:40:46 -0700

    added build script

---
+++ docs.renegade.fi/scripts/build.sh
@@ -0,0 +1,3 @@
+./scripts/download-fonts.sh testnet-fonts us-east-2 "FAMAime-Regular.woff2 FAMAime-Bold.woff2 ABCFavoritMono-Regular.ttf ABCFavoritExtendedVariable.woff2 ABCFavoritExpanded-Regular-Named.ttf ABCFavorit-Regular-Named.ttf ABCFavorit-Light-Named.ttf"
+
+npm run build
